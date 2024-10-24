66e6b7a37c77846f7aca14aa6e17240730bbe892
Abhijit Patil <69165799+abhijit-23blaze@users.noreply.github.com>
2024-10-24 11:16:29 +0530

    club names added

--- src/app/components/SessionScheduleBox.jsx
+++ src/app/components/SessionScheduleBox.jsx
@@ -30,7 +30,23 @@ export default function SessionScheduleBox({ data, classNames }) {
 							data.type === "Connexion" &&
 								"bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-300",
 							data.type === "Nexsynch" &&
-								"bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-300"
+								"bg-Cyan-100 text-Cyan-800 dark:bg-Cyan-900 dark:text-Cyan-300",
+                            data.type === "Reverb" &&
+								"bg-orange-100 text-orange-800 dark:bg-orange-900 dark:text-orange-300",
+                            data.type === "Keynote" &&
+								"bg-violet-100 text-violet-800 dark:bg-violet-900 dark:text-violet-300" ,   
+                            data.type === "Meraki" &&
+								"bg-lime-100 text-lime-800 dark:bg-lime-900 dark:text-lime-300"  ,
+                            data.type === "Beatripperz" &&
+								"bg-teal-100 text-teal-800 dark:bg-teal-900 dark:text-teal-300"  ,
+                            data.type === "Nirvana" &&
+								"bg-amber-100 text-amber-800 dark:bg-amber-900 dark:text-amber-300" ,   
+                            data.type === "FStops" &&
+								"bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-300",
+                            data.type === "Mise-en-Scene" &&
+								"bg-indigo-100 text-indigo-800 dark:bg-indigo-900 dark:text-indigo-300"  ,
+                            data.type === "Fitness" &&
+								"bg-rose-100 text-rose-800 dark:bg-rose-900 dark:text-rose-300"                  
 						)}
 					>
 						{data.type}
